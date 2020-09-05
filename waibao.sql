94879616d9d3312dcc7ea91530a295d2d31117a3
caihanyu <iclzr@outlook.com>
2020-09-05 13:44:57 +0800

    更新了sql测试数据

--- waibao.sql
+++ waibao.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 05/09/2020 10:39:03
+ Date: 05/09/2020 13:43:45
 */
 
 SET NAMES utf8mb4;
@@ -34,39 +34,48 @@ CREATE TABLE `project`  (
   `recruit` int(2) NOT NULL COMMENT '招募类型;\r\n0=\"项目顾问\"\r\n1=\"项目经理\" \r\n2=\"产品经理\" \r\n3=\"Android开发\"\r\n4=\"iOS开发\"\r\n5=\"前端开发\"\r\n6=\"后端开发\"\r\n7=\"全栈开发\"\r\n8=\"测试工程师\"',
   `time` datetime(0) NOT NULL COMMENT '项目创建时间',
   PRIMARY KEY (`projectID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of project
 -- ----------------------------
-INSERT INTO `project` VALUES (1, 'p1', 18645451111, 1, 'details', 4000, 40, 2, '18645451178;', 1, '2020-07-09 07:40:12');
-INSERT INTO `project` VALUES (2, 'p2', 18645451111, 1, 'details', 4000, 40, 1, '18645451178;', 1, '2020-07-09 07:41:12');
-INSERT INTO `project` VALUES (3, 'tp1', 18645451111, 0, 'fabuceshi1', 1000, 7, 0, '', 1, '2020-07-09 03:42:15');
-INSERT INTO `project` VALUES (4, 'tp2', 18645451111, 0, 'fabuceshi2', 1000, 7, 0, '', 1, '2020-07-09 03:43:36');
-INSERT INTO `project` VALUES (5, 'tp3', 18645451111, 1, 'fabuces3', 3000, 30, 0, '', 4, '2020-07-09 03:44:03');
-INSERT INTO `project` VALUES (6, 'tp4', 18645451111, 2, 'fabuces4', 5000, 30, 0, '', 6, '2020-07-09 03:45:02');
-INSERT INTO `project` VALUES (7, 'tp5', 18645451111, 4, 'tp5', 2000, 7, 0, '', 8, '2020-07-09 03:46:24');
-INSERT INTO `project` VALUES (8, 'tp6', 18645451111, 3, 'tp6', 2000, 5, 0, '', 1, '2020-07-09 03:47:06');
-INSERT INTO `project` VALUES (9, '项目发布测试1', 18645451111, 4, '这是一条项目发布测试1', 2222, 22, 0, '', 2, '2020-07-13 08:24:04');
-INSERT INTO `project` VALUES (10, '项目发布测试2', 18645451111, 1, '项目发布测试2', 3000, 20, 0, '18645451178;', 1, '2020-07-13 08:24:35');
-INSERT INTO `project` VALUES (11, '项目发布测试3', 18645451111, 4, '项目发布测试3', 3000, 10, 2, '18645451178;', 4, '2020-07-13 08:24:59');
+INSERT INTO `project` VALUES (1, '发布项目01', 18645451111, 0, '这是项目描述', 1000, 3, 0, '', 0, '2020-09-05 04:30:27');
+INSERT INTO `project` VALUES (2, '发布项目02', 18645451111, 0, '这是项目描述', 3000, 7, 0, '', 4, '2020-09-05 04:36:46');
+INSERT INTO `project` VALUES (3, '发布项目03', 18645451111, 1, '这是项目描述', 4000, 5, 0, '', 6, '2020-09-05 04:37:29');
+INSERT INTO `project` VALUES (4, '发布项目04', 18645451111, 2, '这是项目描述', 5000, 20, 0, '18645451178;18645451179;', 7, '2020-09-05 04:42:06');
+INSERT INTO `project` VALUES (5, 'P1', 18645451112, 3, '这是项目描述', 4000, 20, 0, '', 5, '2020-09-05 05:38:16');
+INSERT INTO `project` VALUES (6, 'P2', 18645451112, 4, '这是项目描述', 3000, 20, 0, '', 5, '2020-09-05 05:38:43');
+INSERT INTO `project` VALUES (7, 'P3', 18645451112, 2, '这是项目描述', 6000, 30, 0, '', 3, '2020-09-05 05:39:13');
+INSERT INTO `project` VALUES (8, 'P4', 18645451112, 0, '这是项目描述', 10000, 30, 0, '', 7, '2020-09-05 05:39:50');
+INSERT INTO `project` VALUES (9, 'T1', 18645451112, 3, '这是项目描述', 3000, 15, 0, '', 4, '2020-09-05 05:40:18');
+INSERT INTO `project` VALUES (10, 'T2', 18645451112, 0, '这是项目描述', 2000, 7, 0, '', 8, '2020-09-05 05:40:41');
+INSERT INTO `project` VALUES (11, 'A1', 18645451112, 2, '这是项目描述', 2000, 10, 0, '', 1, '2020-09-05 05:41:11');
+INSERT INTO `project` VALUES (12, 'A2', 18645451112, 4, '这是项目描述', 3000, 10, 0, '', 5, '2020-09-05 05:41:27');
+INSERT INTO `project` VALUES (13, 'B1', 18645451112, 3, '这是项目描述', 4000, 15, 0, '', 2, '2020-09-05 05:41:48');
+INSERT INTO `project` VALUES (14, 'B2', 18645451112, 0, '这是项目描述', 15000, 30, 0, '', 7, '2020-09-05 05:42:20');
+INSERT INTO `project` VALUES (15, 'B3', 18645451112, 1, '这是项目描述', 2000, 15, 0, '', 1, '2020-09-05 05:42:36');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
-  `birthday` date NULL DEFAULT NULL COMMENT '生日',
-  `sex` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
-  `address` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `userName` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '唯一',
+  `PN` bigint(11) NOT NULL COMMENT '唯一',
+  `pwd` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '唯一',
+  `userStatus` int(1) NOT NULL COMMENT '0=“开发者”\r\n1=“需求方‘',
+  `projectID` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '参与的所有项目的ID',
+  PRIMARY KEY (`PN`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, '罗祥', '1991-04-04', '男', '北京海淀');
+INSERT INTO `user` VALUES ('iuytt', 18645451111, '123456', '22@22.com', 1, '');
+INSERT INTO `user` VALUES ('test2', 18645451112, '123456', '23@22.com', 1, '');
+INSERT INTO `user` VALUES ('iclzr', 18645451178, '123456', '11@11.com', 0, '4;');
+INSERT INTO `user` VALUES ('test1', 18645451179, '123456', '12@11.com', 0, '4;');
+INSERT INTO `user` VALUES ('test3', 18645451180, '123456', '13@11.com', 0, '');
 
 SET FOREIGN_KEY_CHECKS = 1;
